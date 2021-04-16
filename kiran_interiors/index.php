d04c9e2548a682e6ee71de74050b385165d12737
Kiran12354 <62467544+Kiran12354@users.noreply.github.com>
2021-04-16 19:59:35 +0530

    Add files via upload

---
+++ kiran_interiors/index.php
@@ -0,0 +1,274 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<style>
+#card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  max-width: 300px;
+  margin: auto;
+  text-align: center;
+  font-family: arial;
+}
+
+.title {
+  color: grey;
+  font-size: 18px;
+}
+
+#button {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 8px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+  font-size: 18px;
+}
+
+#a {
+  text-decoration: none;
+  font-size: 22px;
+  color: black;
+}
+
+#button:hover, a:hover {
+  opacity: 0.7;
+}
+</style>
+    <title>Interiors</title>
+  </head>
+  <body class="jumbotron" style="background-color: whitesmoke" >
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar" style="float:right">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+        <a class="navbar-brand" href="#" style="color:orange"><h4 style="font-family: cursive;" ><b>My Interiors</b></h4></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav">
+            <li><a href="#myCarousel"><h4>Home</h4></a></li>
+            <li><a href="#process"><h4>Our Process</h4></a></li>
+            <li><a href="#team"><h4>Our Team</h4></a></li>
+            <li><a href="#contact"><h4>Contact Us</h4></a></li>
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item active">
+        <img src="http://cdn.home-designing.com/wp-content/uploads/2019/10/grey-modern-sofa-1.jpg" alt="img-1" style="width:100%;height: 300px">
+        <div class="carousel-caption">
+          <h1 style="font-family: cursive">Living Room</h1>
+          
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="https://greeen.sg/wp-content/uploads/2020/04/green-office-interior-design-ideas-9.jpg" alt="img-2" style="width:100%;height: 300px">
+        <div class="carousel-caption">
+          <h1 style="font-family: cursive">Office</h1>
+         
+        </div>
+      </div>
+    
+      <div class="item">
+        <img src="https://positivefox.com/wp-content/uploads/2017/08/Bedroom-Interior-Design-Ideas-featured-730x548.jpg" alt="img-3" style="width:100%;height: 300px">
+        <div class="carousel-caption">
+          <h1 style="font-family: cursive">Bed Room</h1>
+          
+        </div>
+      </div>
+  
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+      <br>
+      <div class="container">
+      <div class="row">
+          <div class="col-md-6">
+              <br>
+              <br>
+              <img height="100%" width="100%" src="https://www.mydomaine.com/thmb/KWYFPjZMCuhABZNIM5F-o-eiwlM=/735x0/cdn.cliqueinc.com__cache__posts__212361__-2030968-1483470364.700x0c-8571e60cad7b42a981ab29ae10b5c153-1c3248487c784cd2994c5a3ba02f7115.jpg">
+          </div> 
+          <div class="col-md-6" >
+              <h2 style="font-size: 50px;text-align:center"><b>What makes we the best in the interior design</b></h2>
+              <p style="color:grey">Our long history of family ownership has provided financial reassurance, business continuity and stability. It has also created firm foundations from which to promote British design as we expand into new territories. And now, as an established international.</p>
+              <h4><i class="fa fa-arrow-right" style="color:orange"></i>&nbsp;<b>Guaranteed Work</b></h4>
+              <h4><i class="fa fa-arrow-right" style="color:orange"></i>&nbsp;<b>Interior Expertise</b></h4>
+              <h4><i class="fa fa-arrow-right" style="color:orange"></i>&nbsp;<b>Free Consulting</b></h4>
+              <h4><i class="fa fa-arrow-right" style="color:orange"></i>&nbsp;<b>Project management</b></h4>
+          </div> 
+      </div>
+          
+          <hr style="height:2px;border-width:0;color:gray;background-color:gray">
+          <div class="row" id="process">
+              <h2 style="font-size: 40px;"><b>Our Process</b></h2>
+              <br>
+              <div class="col-md-4" style="padding:5px;">
+                  <div class="card" style="background-color:#e6e6e6;text-align: center">
+                      <span class="badge" style="font-size:40px; float: left">1</span>
+                      <br>
+                      <h3><b>Client Discussion</b></h3>
+                      <p style="color: #666666">We develop and refine the design, capturing the look, feel and details by creating digital.</p>
+                      <br>               
+                  </div>
+
+              </div>
+              <div class="col-md-4" style="padding:5px;">
+                  <div class="card" style="background-color:#e6e6e6;text-align: center">
+                      <span class="badge" style="font-size:40px; float: left">2</span>
+                      <br>
+                      <h3><b>Strategy</b></h3>
+                      <p style="color: #666666">We immerse ourselves in your space, attune ourselves to your needs and listen carefully.</p>
+                      <br>               
+                  </div>
+
+              </div>
+              <div class="col-md-4" style="padding:5px;">
+                  <div class="card" style="background-color:#e6e6e6;text-align: center">
+                      <span class="badge" style="font-size:40px; float: left">3</span>
+                      <br>
+                      <h3><b>Core Concept</b></h3>
+                      <p style="color: #666666">At the end of this step, we have established the vision, goals, budget and timeline.</p>
+                      <br>               
+                  </div>
+
+              </div>
+          </div>
+          <div class="row">
+              <div class="col-md-4" style="padding:5px;">
+                  <div class="card" style="background-color:#e6e6e6;text-align: center">
+                       <span class="badge" style="font-size:40px; float: left">4</span>
+                       <br>
+                      <h3><b>Technical Feasibility</b></h3>
+                      <p style="color: #666666">Presentation renderings and 3D models that enable you to visualize the end result.</p>
+                      <br>               
+                  </div>
+
+              </div>
+              <div class="col-md-4" style="padding:5px;">
+                  <div class="card" style="background-color:#e6e6e6;text-align: center">
+                      <span class="badge" style="font-size:40px; float: left">5</span>
+                      <br>
+                      <h3><b>Execution</b></h3>
+                      <p style="color: #666666">We oversee every aspect as your space takes shape, from demolition to furniture install.</p>
+                      <br>               
+                  </div>
+
+              </div>
+              <div class="col-md-4" style="padding:5px;">
+                  <div class="card" style="background-color:#e6e6e6;text-align: center">
+                      <span class="badge" style="font-size:40px; float: left">6</span>
+                      <br>
+                      <h3><b>Follow Up</b></h3>
+                      <p style="color: #666666">Our extensive experience in <br>construction and project management enable us.</p>
+                      <br>               
+                  </div>
+
+              </div>
+          </div>
+          
+          <br>
+          <hr style="height:2px;border-width:0;color:gray;background-color:gray">
+          <div class="row" id="team">
+           <h2 style="font-size: 40px;margin-left: 15px" > <b>Meet Our Team</b></h2>
+           <br>
+           <div class="col-md-4">
+           <div id="card">
+            <img src="https://www.mydomaine.com/thmb/KWYFPjZMCuhABZNIM5F-o-eiwlM=/735x0/cdn.cliqueinc.com__cache__posts__212361__-2030968-1483470364.700x0c-8571e60cad7b42a981ab29ae10b5c153-1c3248487c784cd2994c5a3ba02f7115.jpg" alt="John" style="width:100%">
+            <h1>John Doe</h1>
+             <p class="title">CEO & Founder, Example</p>
+             <p>Harvard University</p>
+             </div>
+            </div>
+              
+            <div class="col-md-4">
+            <div id="card">
+            <img src="https://www.mydomaine.com/thmb/KWYFPjZMCuhABZNIM5F-o-eiwlM=/735x0/cdn.cliqueinc.com__cache__posts__212361__-2030968-1483470364.700x0c-8571e60cad7b42a981ab29ae10b5c153-1c3248487c784cd2994c5a3ba02f7115.jpg" alt="John" style="width:100%">
+            <h1>John Doe</h1>
+            <p class="title">CEO & Founder, Example</p>
+            <p>Harvard University</p>
+            </div>
+            </div>
+            
+           <div class="col-md-4">
+           <div id="card">
+            <img src="https://www.mydomaine.com/thmb/KWYFPjZMCuhABZNIM5F-o-eiwlM=/735x0/cdn.cliqueinc.com__cache__posts__212361__-2030968-1483470364.700x0c-8571e60cad7b42a981ab29ae10b5c153-1c3248487c784cd2994c5a3ba02f7115.jpg" alt="John" style="width:100%">
+            <h1>John Doe</h1>
+            <p class="title">CEO & Founder, Example</p>
+            <p>Harvard University</p>
+           </div>
+          </div>
+         </div>
+           
+       <br>
+       
+          <hr style="height:2px;border-width:0;color:gray;background-color:gray">
+          
+       
+          <div class="row" style="background-color:black;margin-left: 0px;margin-right: 0px;text-align: center" id="contact">
+              <h2 style="font-size: 40px;color:whitesmoke" ><b>Contact Us</b></h2>    
+              <br>
+              <div class="col-md-1"></div>
+              <div class="col-md-3">
+                  <h3 style="color: whitesmoke;"><b>Get In Touch</b></h3>
+                 <h4 style="color: #b3b3b3;">My Interiors</h4>
+                 <h4 style="color: #b3b3b3;">Banglore-560102</h4>
+                 <h4 style="color: #b3b3b3;">Phone:-9743608536</h4>
+                 <h4 style="color: #b3b3b3;">Mail:-myinteriors@gmail.com</h4>
+              </div>
+              <div class="col-md-3">
+                  <h3 style="color: whitesmoke;"><b>Services</b></h3>
+                 <h4 style="color: #b3b3b3;">Interior Design</h4>
+                 <h4 style="color: #b3b3b3;">Planning</h4>
+                 <h4 style="color: #b3b3b3;">Lightning</h4>
+              </div>
+              <div class="col-md-5">
+                  <h3 style="color: whitesmoke;"><b>Location</b></h3>
+                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31111.679098394536!2d77.62831303838337!3d12.910299698310421!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1491bfdc6ecd%3A0xf232718439fbc879!2sHSR%20Layout%2C%20Bengaluru%2C%20Karnataka!5e0!3m2!1sen!2sin!4v1618502030067!5m2!1sen!2sin" width="100%" height="300" style="border:0;"></iframe>
+              </div>
+          </div>
+          </div>
+<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  </body>
+</html>
